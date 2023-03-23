9c2cbe7d3960f66f03075224e9200fb8ee628b98
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 15:22:17 +0100

    Add extension 'Negative' for System.Double type

---
+++ Source/Padutronics.Validation.Extensions.System/DoubleVerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class DoubleVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, double> @this)
+    {
+        return @this.LessThan(0.0);
+    }
+}
