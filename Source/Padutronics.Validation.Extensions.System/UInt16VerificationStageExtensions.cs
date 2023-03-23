ab7b30f000c6060ecc855bac4316ab272586945d
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:10:51 +0100

    Add extension 'Zero' for System.UInt16 type

---
+++ Source/Padutronics.Validation.Extensions.System/UInt16VerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class UInt16VerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Zero<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, ushort> @this)
+    {
+        return @this.EqualTo((ushort)0);
+    }
+}
---
+++ Tests/Padutronics.Validation.Extensions.System.Tests/UInt16_is_zero.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Padutronics.Validation.Extensions.System.Tests.Stubs.Models;
+using Padutronics.Validation.Rules.Building;
+
+namespace Padutronics.Validation.Extensions.System.Tests;
+
+[TestFixture]
+internal sealed class UInt16_is_zero
+{
+    private sealed class ModelValidator : Validator<Model<ushort>>
+    {
+        protected override void BuildRuleSet(IRuleSetBuilder<Model<ushort>> ruleSetBuilder)
+        {
+            ruleSetBuilder.Property(model => model.Value)
+                .Is.Zero()
+                .WithMessage("Value must be zero.");
+        }
+    }
+
+    [Test]
+    public void Validation_is_succeeded_if_value_is_zero()
+    {
+        // Arrange.
+        var validator = new ModelValidator();
+        var model = new Model<ushort>(0);
+
+        // Act.
+        ValidationResult result = validator.Validate(model);
+
+        // Assert.
+        Assert.That(result.IsSucceeded, Is.True);
+    }
+
+    [Test]
+    public void Validation_is_failed_if_value_is_positive()
+    {
+        // Arrange.
+        var validator = new ModelValidator();
+        var model = new Model<ushort>(3);
+
+        // Act.
+        ValidationResult result = validator.Validate(model);
+
+        // Assert.
+        Assert.That(result.IsSucceeded, Is.False);
+    }
+}
