5f58f4c67ad6155fffa75e6727477f2905762e96
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:55:32 +0100

    Add extension 'Negative' for System.Int64 type

---
+++ Source/Padutronics.Validation.Extensions.System/Int64VerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class Int64VerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, long> @this)
+    {
+        return @this.LessThan(0L);
+    }
+}
