89dd01bcb9a72ad652c4be6fa7ffb5e5e1fad3c0
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 15:25:49 +0100

    Add extension 'Negative' for System.Single type

---
+++ Source/Padutronics.Validation.Extensions.System/SingleVerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class SingleVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, float> @this)
+    {
+        return @this.LessThan(0.0F);
+    }
+}
