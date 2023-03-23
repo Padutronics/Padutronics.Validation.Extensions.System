e76c41ab1dcef15e0d3d52781b5de7dd3599d348
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:08:32 +0100

    Add extension 'False' for System.Boolean type

---
+++ Source/Padutronics.Validation.Extensions.System/BooleanVerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class BooleanVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> False<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, bool> @this)
+    {
+        return @this.EqualTo(false);
+    }
+}
