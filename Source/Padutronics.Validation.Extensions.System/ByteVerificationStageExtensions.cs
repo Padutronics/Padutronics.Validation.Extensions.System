ce1c1842a817efc5abe030cc2bf1729410c58773
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:10:01 +0100

    Add extension 'Zero' for System.Byte type

---
+++ Source/Padutronics.Validation.Extensions.System/ByteVerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class ByteVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Zero<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, byte> @this)
+    {
+        return @this.EqualTo((byte)0);
+    }
+}
---
+++ Tests/Padutronics.Validation.Extensions.System.Tests/Byte_is_zero.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Padutronics.Validation.Extensions.System.Tests.Stubs.Models;
+using Padutronics.Validation.Rules.Building;
+
+namespace Padutronics.Validation.Extensions.System.Tests;
+
+[TestFixture]
+internal sealed class Byte_is_zero
+{
+    private sealed class ModelValidator : Validator<Model<byte>>
+    {
+        protected override void BuildRuleSet(IRuleSetBuilder<Model<byte>> ruleSetBuilder)
+        {
+            ruleSetBuilder.Property(model => model.Value)
+                .Is.Zero()
+                .WithMessage("Value must be zero.");
+        }
+    }
+
+    [Test]
+    public void Validation_is_succeeded_if_value_is_zero()
+    {
+        // Arrange.
+        var validator = new ModelValidator();
+        var model = new Model<byte>(0);
+
+        // Act.
+        ValidationResult result = validator.Validate(model);
+
+        // Assert.
+        Assert.That(result.IsSucceeded, Is.True);
+    }
+
+    [Test]
+    public void Validation_is_failed_if_value_is_positive()
+    {
+        // Arrange.
+        var validator = new ModelValidator();
+        var model = new Model<byte>(3);
+
+        // Act.
+        ValidationResult result = validator.Validate(model);
+
+        // Assert.
+        Assert.That(result.IsSucceeded, Is.False);
+    }
+}
