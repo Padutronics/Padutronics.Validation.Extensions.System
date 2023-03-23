e3c3aeccd2ba7ef173e446a634c6e2a5cce43c95
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:12:27 +0100

    Add extension 'Zero' for System.UInt64 type

---
+++ Source/Padutronics.Validation.Extensions.System/UInt64VerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class UInt64VerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Zero<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, ulong> @this)
+    {
+        return @this.EqualTo(0UL);
+    }
+}
