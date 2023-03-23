d9ec6d2c1d5667a0a72b44819732e6874c3823a2
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 15:26:33 +0100

    Add extension 'NegativeOrZero' for System.Single type

--- Source/Padutronics.Validation.Extensions.System/SingleVerificationStageExtensions.cs
+++ Source/Padutronics.Validation.Extensions.System/SingleVerificationStageExtensions.cs
@@ -8,4 +8,9 @@ public static class SingleVerificationStageExtensions
     {
         return @this.LessThan(0.0F);
     }
+
+    public static IConditionStage<TRuleChainBuilder, TTarget> NegativeOrZero<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, float> @this)
+    {
+        return @this.LessThanOrEqualTo(0.0F);
+    }
 }
