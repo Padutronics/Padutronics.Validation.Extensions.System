c18bfd0c7293a604b083610daa4193eb804fcb17
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:27:00 +0100

    Add extension 'Negative' for System.SByte type

---
+++ Source/Padutronics.Validation.Extensions.System/SByteVerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class SByteVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, sbyte> @this)
+    {
+        return @this.LessThan((sbyte)0);
+    }
+}
