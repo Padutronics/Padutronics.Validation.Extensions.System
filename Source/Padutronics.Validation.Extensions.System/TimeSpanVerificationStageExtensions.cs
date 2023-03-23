1c58cad06e0cc951848579e6c5921f6848f4a328
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 15:29:57 +0100

    Add extension 'Negative' for System.TimeSpan type

---
+++ Source/Padutronics.Validation.Extensions.System/TimeSpanVerificationStageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+using System;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class TimeSpanVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, TimeSpan> @this)
+    {
+        return @this.LessThan(TimeSpan.Zero);
+    }
+}
