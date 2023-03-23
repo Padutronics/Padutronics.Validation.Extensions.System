d17d70fad1406033a65b3ad1e876cb7acbd86c4f
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:31:27 +0100

    Add extension 'Negative' for System.Int16 type

---
+++ Source/Padutronics.Validation.Extensions.System/Int16VerificationStageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Padutronics.Validation.Rules.Building.Fluent;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class Int16VerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> Negative<TRuleChainBuilder, TTarget>(this IVerificationStage<TRuleChainBuilder, TTarget, short> @this)
+    {
+        return @this.LessThan((short)0);
+    }
+}
