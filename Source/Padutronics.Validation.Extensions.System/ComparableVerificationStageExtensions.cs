3025a2475ce17fc97e7076660e510e83eca9b81f
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-23 14:14:27 +0100

    Add extension 'GreaterThan' for System.IComparable`1 type

---
+++ Source/Padutronics.Validation.Extensions.System/ComparableVerificationStageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Padutronics.Validation.Extensions.System.Verifiers;
+using Padutronics.Validation.Extensions.System.Verifiers.Adapters;
+using Padutronics.Validation.Rules.Building.Fluent;
+using System;
+
+namespace Padutronics.Validation.Extensions.System;
+
+public static class ComparableVerificationStageExtensions
+{
+    public static IConditionStage<TRuleChainBuilder, TTarget> GreaterThan<TRuleChainBuilder, TTarget, TValue>(this IVerificationStage<TRuleChainBuilder, TTarget, TValue> @this, TValue lowerBound)
+        where TValue : IComparable<TValue>
+    {
+        return @this.VerifiableBy(new GreaterComparableVerifier<TValue>(lowerBound));
+    }
+
+    public static IConditionStage<TRuleChainBuilder, TTarget> GreaterThan<TRuleChainBuilder, TTarget, TValue>(this IVerificationStage<TRuleChainBuilder, TTarget, TValue> @this, Func<TTarget, TValue> lowerBoundExtractor)
+        where TValue : IComparable<TValue>
+    {
+        return @this.VerifiableBy(
+            new VerifierFactoryToVerifierAdapter<TTarget, TValue>(
+                target => new GreaterComparableVerifier<TValue>(lowerBoundExtractor(target))
+            )
+        );
+    }
+}
---
+++ Source/Padutronics.Validation.Extensions.System/Verifiers/GreaterComparableVerifier.cs
@@ -0,0 +1,23 @@
+﻿using Padutronics.Extensions.System;
+using Padutronics.Validation.Verifiers;
+using System;
+
+namespace Padutronics.Validation.Extensions.System.Verifiers;
+
+internal sealed class GreaterComparableVerifier<T> : Verifier<T>
+    where T : IComparable<T>
+{
+    private readonly T lowerBound;
+
+    public GreaterComparableVerifier(T lowerBound)
+    {
+        this.lowerBound = lowerBound;
+    }
+
+    public override VerificationResult Verify(T value)
+    {
+        return value.IsGreaterThan(lowerBound)
+            ? VerificationResults.Success
+            : VerificationResults.Failure;
+    }
+}
